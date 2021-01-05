e7626ca5563f74bfeb54d1ab8b1f99eddd1fe214
takoikatakotako <snorlax.chemist.and.jazz@gmail.com>
2021-01-05 17:31:50 +0900

    掲示板を作成

--- src/main/java/com/swiswiswift/forum/ForumController.java
+++ src/main/java/com/swiswiswift/forum/ForumController.java
@@ -2,6 +2,7 @@ package com.swiswiswift.forum;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
@@ -15,26 +16,28 @@ public class ForumController {
 
     private final PostRepository postRepository;
 
-
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public String healthCheck() {
+    public String healthCheck(Model model) {
+        ArrayList<Post> posts = new ArrayList<>(postRepository.findAll());
+        model.addAttribute("posts", posts);
         return "index";
     }
 
     @ResponseBody
     @RequestMapping(value = "/api/list", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
-    public ArrayList<Post> getPostList() {
+    public ArrayList<Post> getPostList(HttpServletResponse response) {
         return new ArrayList<>(postRepository.findAll());
     }
 
     @ResponseBody
     @RequestMapping(value = "/api/add", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
-    public String addPost(@RequestBody PostRequestBean postRequestBean) {
+    public String addPost(@RequestBody PostRequestBean postRequestBean, HttpServletResponse response) {
         Post post = new Post();
         post.setNickname(postRequestBean.getNickname());
         post.setMessage(postRequestBean.getMessage());
         post.setPostDatetime(LocalDateTime.now());
         postRepository.save(post);
+        response.setStatus(HttpServletResponse.SC_OK);
         return "Success";
     }
 }
